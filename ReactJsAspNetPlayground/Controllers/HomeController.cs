66bd65b6b542d08666ea231842a50c041cdfdb4d
Lukasz Z Sadlocha <44022837@HSBC>
2017-03-22 13:00:54 +0100

    second commit

--- ReactJsAspNetPlayground/Controllers/HomeController.cs
+++ ReactJsAspNetPlayground/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace ReactJsAspNetPlayground.Controllers
         {
             return View();
         }
+
+        public ActionResult ContactList()
+        {
+            return View();
+        }
     }
 }
