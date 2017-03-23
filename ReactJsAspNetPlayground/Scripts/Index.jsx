88c46fc46e78ac4f9817904f65de28f2f76f25d6
lukaszsadlocha <lukaszsadlocha@gmail.com>
2017-03-23 14:18:30 +0100

    add bootstrap sass

--- ReactJsAspNetPlayground/Scripts/Index.jsx
+++ ReactJsAspNetPlayground/Scripts/Index.jsx
@@ -1,1 +1,15 @@
-﻿
+﻿var Table = Reactable.Table;
+
+var data = [
+        { Name: 'Griffin Smith', Age: 18, Position: 'Developer' },
+        { Age: 23, Name: 'Lee Salminen', Position: 'Policeman' },
+        {Name: 'Adam Malysz', Age: 28, Position: 'Ski jumper' },
+        {Name: 'Lukasz Zieba', Age: 32, Position: 'Web Lead' },
+        {Name: 'Lukasz Sadlocha', Age: 26, Position: 'Junior Dev' },
+    ];
+
+
+ReactDOM.render(
+    <Table className="table" data={data} itemsPerPage={3} />,
+    document.getElementById('root')
+);
